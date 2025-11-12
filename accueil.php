91c28252dc09a50273bce272d01337838f8b94bb
Laura Le Gall <laura.maglegall@gmail.com>
2025-11-12 16:07:21 +0100

    files creation, html for main page

--- accueil.php
+++ accueil.php
@@ -1,5 +1,6 @@
 <?php
 
+//get the data infomation
 $serveurName ='localhost';
 $id = "root";
 $password="root";
@@ -9,6 +10,9 @@ $conn= new PDO("mysql:host=$serveurName;dbname=tp_netflixx_LEGALL_LAURA", $id,$p
     echo "erreur" .$e->getMessage();
 }
 
+$getMovies = $conn->prepare("SELECT * FROM (SELECT * FROM movies ORDER BY id DESC LIMIT 5) x ORDER BY id ASC");
+$getMovies->execute();
+$result=$getMovies->fetchAll();
 
 ?>
 
@@ -19,17 +23,50 @@ $conn= new PDO("mysql:host=$serveurName;dbname=tp_netflixx_LEGALL_LAURA", $id,$p
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Acceuil</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
+<header>
+    <nav>
+        <div>
+            <img src="assets/main.png" alt="" width=150px>
+        </div>
+        
+        <ul>
+            <div class="explore">  
+                <li> <a href="accueil.php"> Accueil</a></li>
+                <li> <a href="moviesList.php"> Tous nos films</a></li>
+                <li> <a href="admin.php"> Espace Admin</a></li>
+               
+            </div>
+            <div class="connect">
+                <li><a href="inscription.php"> Inscription / </a></li>
+                <li><a href="connection.php"> Connection</a></li>
+            </div>
 
+             <div class="deconnect">  
+                 <li> <a href="#"> Me déconnecter </a></li>
+            </div>
+           
+        </ul>
+    </nav>
+</header>
 
-<h1>Bonjour, quels films souhaites tu regarder ?</h1>
-<ul>
-    <li></li>
-    <li></li>
-    </li></li>
-    <li></li>
-</ul>
+<main>
+    <h1>Bonjour et bienvenue sur PixiePop </h1>
+    <p>Votre site magique pour regarder vos films Disney préférés seul(e) ou accompagné(e) ! </p>
+    <p>Explorez un univers féerique, regardez vos classiques ou vos nouveautés préférées. Pour ajouter des films et profiter pleinement de notre collection, créez votre compte et rejoignez la magie !</p>
+
+    
+
+<!-- foreach lign of data, get the picture title and trailer-->
+<?php foreach($result as $movie): ?>
+    <h2><?php echo $movie['title']; ?></h2>
+    <p><?php echo $movie['description']; ?></p>
+    <img src="assets/<?php echo $movie['urlphoto']; ?>" alt="">
+<?php endforeach; ?>
+
+</main>
     
 </body>
 </html>
